2ddd11449cc334e7201d3811b547707e2d663e44
Dominic May <jack.thatch@gmail.com>
2013-10-10 23:48:29 +0800

    Reinstate transforms

--- src/js/app/data_utils/transforms.js
+++ src/js/app/data_utils/transforms.js
@@ -1,10 +1,10 @@
-// App.RawTransform = DS.Transform.extend({
-//   deserialize: function(serialized) {
-//     return serialized;
-//   },
-//   serialize: function(deserialized) {
-//     return deserialized;
-//   }
-// });
+App.RawTransform = DS.Transform.extend({
+  deserialize: function(serialized) {
+    return serialized;
+  },
+  serialize: function(deserialized) {
+    return deserialized;
+  }
+});
 
-// App.ArrayTransform = App.RawTransform.extend({});
+App.ArrayTransform = App.RawTransform.extend({});
