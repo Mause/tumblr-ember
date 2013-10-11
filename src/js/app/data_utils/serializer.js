d00208d3b01f97a533166783a82cb261cb5507c8
Dominic May <jack.thatch@gmail.com>
2013-10-11 16:37:46 +0800

    Rewrote serializer for flexibility, and to ensure metadata is passed through

--- src/js/app/data_utils/serializer.js
+++ src/js/app/data_utils/serializer.js
@@ -1,4 +1,6 @@
 App.ApplicationSerializer = DS.RESTSerializer.extend({
+  extractMeta: function(store, type, payload){},
+
   generateIds: function(records, parent_id) {
     for (var i=0; i<records.length; i++) {
       records[i].id = '%@.%@'.fmt(parent_id, i);
@@ -7,18 +9,70 @@ App.ApplicationSerializer = DS.RESTSerializer.extend({
   },
 
   extractArray: function(store, type, payload, id, requestType) {
-    payload = this.sortPostArray(store, type, payload, id, requestType);
-    return this.extractEmbedded(store, type, payload, id, requestType);
+    var metadata = payload.meta;
+    delete payload.metadata;
+
+    payload = this.splitPostArray(store, type, payload, id, requestType);
+    payload = this.extractEmbedded(store, type, payload, id, requestType);
+    payload = this.normalizeArray(store, type, payload, id, requestType);
+    payload = this.transmogrify(store, type, payload, id, requestType);
+
+    payload.metadata = metadata;
+    return payload;
   },
 
-  sortPostArray: function(store, type, payload, id, requestType) {
+  transmogrify: function(store, type, payload, id, requestType){
+    'use strict';
+    var all_posts = [],
+        this_type,
+        cur_posts,
+        hash,
+        top_level_posts;
+
+    for (var key in payload){
+      if (!payload.hasOwnProperty(key)) { continue; }
+
+      hash = payload[key];
+      this_type = this.typeForRoot(key);
+
+      cur_posts = store.pushMany(this_type, hash);
+      all_posts = all_posts.concat(cur_posts);
+    }
+
+    all_posts = all_posts.sortBy('timestamp');
+    top_level_posts = all_posts.filter(this.filter);
+
+    return top_level_posts;
+  },
+
+  filter: function(item){
+    return ['photo', 'video', 'quote', 'text'].contains(item.get('type'));
+  },
+
+  normalizeArray: function(store, primaryType, payload) {
+    payload = this.normalizePayload(primaryType, payload);
+
+    for (var prop in payload) {
+      var typeName = this.typeForRoot(prop),
+          type = store.modelFor(typeName),
+          typeSerializer = store.serializerFor(type);
+
+      /*jshint loopfunc:true*/
+      payload[prop] = payload[prop].map(function(hash) {
+        return typeSerializer.normalize(type, hash, prop);
+      }, this);
+
+    }
+    return payload;
+  },
+
+  splitPostArray: function(store, type, payload, id, requestType) {
     'use strict';
     var unsorted_posts = payload.response.posts,
         posts = {},
         post,
         post_type;
-    delete payload.response;
-    delete payload.meta;
+    payload = {};
 
     for (var i=0; i<unsorted_posts.length; i++){
       post = unsorted_posts[i];
@@ -36,49 +90,98 @@ App.ApplicationSerializer = DS.RESTSerializer.extend({
 
   extractEmbedded: function(store, type, payload, id, requestType) {
     'use strict';
-    var record,
-        parent_name,
-        embedded_key,
-        embedded_name,
-        current_records,
-        embedded_records,
-        embedded_record_key;
+    var done_keys = [],
+        self=this,
+        done = function(pTN){ return !done_keys.contains(pTN); },
+        typeNames,
+        typeName,
+        parentType;
 
-    // iterate through the possible embedded keys
-    for (var i=0; i<this.embedded_records_keys.length; i++) {
-      embedded_record_key = this.embedded_records_keys[i];
-
-      parent_name = embedded_record_key[0];
-      embedded_name = embedded_record_key[1];
-      embedded_key = embedded_record_key[2];
-
-      if (!payload.hasOwnProperty(parent_name)){
-        continue;
+    while (true){
+      typeNames = Ember.keys(payload).filter(done);
+     if (Em.isEmpty(typeNames)){
+        break;
+      } else {
+        typeName = typeNames.objectAt(0);
       }
 
-      embedded_records = [];
-      for (var q=0; q<payload[parent_name].length; q++) {
-        record = payload[parent_name][q];
+      parentType = this.store.modelFor(typeName);
+      /*jshint loopfunc:true*/
+      parentType.eachRelationship(function(name, meta){
+        Ember.debug('extracting records from the %@ attribute from %@ record'.fmt(
+          name, typeName));
+        payload = self.process(payload, typeName, name, meta);
+      });
 
-        current_records = this.generateIds(record[embedded_key], record.id);
-
-        record[embedded_key] = current_records.mapBy('id');
-
-        embedded_records = embedded_records.concat(current_records);
-      }
-
-      if (!payload.hasOwnProperty(embedded_name)){
-        payload[embedded_name] = [];
-      }
-
-      payload[embedded_name] = payload[embedded_name].concat(embedded_records);
+      done_keys.push(typeName);
     }
+
 
     return payload;
   },
 
-  embedded_records_keys: [
-    ['photo', 'sub_photo', 'photos'],
-    ['sub_photo', 'sub_photo_instance', 'alt_sizes']
-  ]
+  process: function(payload, parentTypeName, name, meta){
+    'use strict';
+
+    if (meta.kind === 'belongsTo' && typeof payload[parentTypeName][0][name] === 'string'){
+      // the relationship is already id-based
+      return;
+    }
+
+    var embedded_records = [],
+        current_records,
+        record;
+
+    for (var q=0; q<payload[parentTypeName].length; q++) {
+      // grab the record
+      record = payload[parentTypeName][q];
+
+      if (Em.typeOf(record[name]) === 'object'){
+        // belongsTo relationship
+        record[name].id = '%@.%@'.fmt(record.id, name);
+
+        embedded_records.push(record[name]);
+
+        record[name] = record[name].id;
+      } else {
+        // hasMany relationship
+
+        // generate ids for each of the embedded records
+        current_records = this.generateIds(record[name], record.id);
+
+        // grab the generated ids, insert into the record
+        record[name] = current_records.mapBy('id');
+
+        // store the embedded records for processing
+        embedded_records = embedded_records.concat(current_records);
+      }
+    }
+
+    if (!payload.hasOwnProperty(meta.type.typeKey)){
+      payload[meta.type.typeKey] = [];
+    }
+
+    payload[meta.type.typeKey] = payload[meta.type.typeKey].concat(
+      embedded_records);
+
+    return payload;
+  }
+});
+
+DS.AdapterPopulatedRecordArray.reopen({
+  load: function(records){
+    metadata = records.metadata;
+    delete records.metadata;
+
+    if (Ember.get(this, 'type') === App.Post){
+      this.setProperties({
+        content: Ember.A(records),
+        isLoaded: true,
+        meta: metadata
+      });
+      Ember.run.once(this, 'trigger', 'didLoad');
+    } else {
+      return this._super.call(this, records);
+    }
+  }
 });
