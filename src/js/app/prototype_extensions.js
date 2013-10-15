2ec4ec2026f5e768956e7b91912066778f511899
Dominic May <jack.thatch@gmail.com>
2013-10-15 20:05:54 +0800

    Added Function.prototype.debounce shortcut

--- src/js/app/prototype_extensions.js
+++ src/js/app/prototype_extensions.js
@@ -25,3 +25,11 @@ Ember.Enumerable.reopen({
   sum: Array.prototype.sum,
   sortBy: Array.prototype.sortBy
 });
+
+Function.prototype.debounce = function(time){
+  var func = this;
+
+  return function(){
+    Em.run.debounce(this, func, time);
+  };
+};
