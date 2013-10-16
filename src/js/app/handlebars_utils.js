cc3ebdf76525d2373b81dada38e4ac1e6112c551
Dominic May <jack.thatch@gmail.com>
2013-10-16 22:26:17 +0800

    Added switch template cache

--- src/js/app/handlebars_utils.js
+++ src/js/app/handlebars_utils.js
@@ -1,25 +1,34 @@
-Ember.Handlebars.registerHelper('switch', function(options){
-  var mapping = { photo: 'photoset' },
-    attribute = options.hash['attribute'],
-    io = attribute.indexOf('.'),
+(function(){
+  var mapping = { photo: 'photoset' };
 
-    context = (options.contexts && options.contexts.length) ? options.contexts[0] : this,
-    normalized = Ember.Handlebars.normalizePath(context, attribute, options.data),
-    obj = normalized.root,
+  var template_cache = {};
 
-    template,
-    attr_val;
 
-  Em.assert('You must provide both an obj and an attribute on that object', !!obj && !! attribute);
-  Em.assert('The attribute path must contain a dot', io >= 0);
-  Em.assert('The attribute must be on the object', attribute.substring(0, io) === options.hash['obj']);
+  Ember.Handlebars.registerHelper('switch', function(options){
+    var attribute = options.hash['attribute'],
+      io = attribute.indexOf('.'),
 
-  attribute = attribute.substring(io + 1, attribute.length);
-  Em.assert('The attribute must exist on the object', !!obj.get(attribute));
+      context = (options.contexts && options.contexts.length) ? options.contexts[0] : this,
+      normalized = Ember.Handlebars.normalizePath(context, attribute, options.data),
+      obj = normalized.root,
 
-  attr_val = obj.get(attribute);
-  attr_val = mapping[attr_val] || attr_val;
+      template,
+      attr_val;
 
-  template = Ember.Handlebars.compile("{{partial '%@'}}".fmt(attr_val));
-  return template(this, options);
-});
+    Em.assert('You must provide both an obj and an attribute on that object', !!obj && !! attribute);
+    Em.assert('The attribute path must contain a dot', io >= 0);
+    Em.assert('The attribute must be on the object', attribute.substring(0, io) === options.hash['obj']);
+
+    attribute = attribute.substring(io + 1, attribute.length);
+    Em.assert('The attribute must exist on the object', !!obj.get(attribute));
+
+    attr_val = obj.get(attribute);
+    attr_val = mapping[attr_val] || attr_val;
+
+    template = template_cache[attr_val] || Ember.Handlebars.compile("{{partial '%@'}}".fmt(attr_val));
+    template_cache[attr_val] = template;
+
+    return template(this, options);
+  });
+
+})(Ember);
