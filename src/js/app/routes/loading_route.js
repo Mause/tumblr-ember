d7e312b42994686031e2efa815cdc648cf82410a
Dominic May <jack.thatch@gmail.com>
2013-10-17 21:00:30 +0800

    The loading template should always be displayed in the main outlet

--- src/js/app/routes/loading_route.js
+++ src/js/app/routes/loading_route.js
@@ -1,3 +1,10 @@
 App.LoadingRoute = Em.Route.extend({
-  templateName: 'loading_route'
+  templateName: 'loading_route',
+
+  renderTemplate: function() {
+    // have it always render into the application template
+    this.render('loading', {
+      outlet: 'main'
+    });
+  }
 });
