1f096367f597b7681e20306c5421fc0657006434
Dominic May <jack.thatch@gmail.com>
2013-10-17 21:45:36 +0800

    Simplified switch handlebars helper :)

--- src/js/app/handlebars_utils.js
+++ src/js/app/handlebars_utils.js
@@ -1,20 +1,12 @@
 Ember.Handlebars.registerHelper('switch', function(options){
   var mapping = { photo: 'photoset' },
-      attribute = options.hash['attribute'],
-      io = attribute.indexOf('.'),
+      attribute = options.hash['attr'],
 
-      context = (options.contexts && options.contexts.length) ? options.contexts[0] : this,
-      normalized = Ember.Handlebars.normalizePath(context, attribute, options.data),
-      obj = normalized.root;
+      normalized = Ember.Handlebars.normalizePath(this, attribute, options.data),
+      obj = normalized.root,
+      attr_val = obj.get(normalized.path);
 
-  Em.assert('You must provide both an obj and an attribute on that object', !!obj && !! attribute);
-  Em.assert('The attribute path must contain a dot', io >= 0);
-  Em.assert('The attribute must be on the object', attribute.substring(0, io) === options.hash['obj']);
-
-  attribute = attribute.substring(io + 1, attribute.length);
-  Em.assert('The attribute must exist on the object', !!obj.get(attribute));
-
-  var attr_val = obj.get(attribute);
+  Em.assert('The attribute must exist on the object', !!attr_val);
   attr_val = mapping[attr_val] || attr_val;
 
   Ember.Handlebars.helpers.partial.call(this, attr_val, options);
