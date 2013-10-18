a9c0913fea92959b8bcc8b17752e873321c57fdc
Dominic May <jack.thatch@gmail.com>
2013-10-18 23:20:17 +0800

    Better url transformation

--- src/js/app/data_utils/transforms.js
+++ src/js/app/data_utils/transforms.js
@@ -24,9 +24,26 @@ App.TumblrStringTransform = DS.StringTransform.extend({
 
     // reformat tumblr urls appropriately
     return deserialized.replace(/href="http:\/\/([^\.]*).tumblr.com\/([^"]*)"/g, function(orig, blog_name, path, idx, full){
-      url = router.router.recognizer.generate('single_blog', {blog_name: blog_name});
-      url = router.get('location').formatURL(url);
+      if (!Em.isEmpty(path)){
+        path = /post\/(\d+)\/(.*)?/.exec(path);
 
+
+        var post_ident = path[1],
+            post_slug = path[2];
+
+        url = router.router.recognizer.generate('single_post', {
+          blog_name: blog_name,
+          post_id: post_ident,
+          post_slug: post_slug
+        });
+
+      } else {
+        url = router.router.recognizer.generate('single_blog', {blog_name: blog_name});
+      }
+
+      console.log('%@ -> %@'.fmt(orig.split('"')[1], url));
+
+      url = router.get('location').formatURL(url);
       if (url.charAt(0) !== '/') { url = '/' + url; }
 
       return 'href="%@"'.fmt(url);
