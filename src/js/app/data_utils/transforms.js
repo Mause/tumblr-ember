ef7400fb621f978183d35cbcb0d7c61e9e688c3a
Dominic May <jack.thatch@gmail.com>
2013-10-19 23:19:36 +0800

    Allow post urls without slugs to be transformed
     * Modify for post_id -> post_ident in route dynamic segment

--- src/js/app/data_utils/transforms.js
+++ src/js/app/data_utils/transforms.js
@@ -25,13 +25,13 @@ App.TumblrStringTransform = DS.StringTransform.extend({
     // reformat tumblr urls appropriately
     return deserialized.replace(/href="http:\/\/([^\.]*).tumblr.com\/([^"]*)"/g, function(orig, blog_name, path, idx, full){
       if (!Em.isEmpty(path)){
-        path = /post\/(\d+)\/(.*)?/.exec(path);
+        path = /post\/(\d+)\/?(.*)?/.exec(path);
         var post_ident = path[1],
             post_slug = path[2];
 
         url = router.router.recognizer.generate('single_post', {
           blog_name: blog_name,
-          post_id: post_ident,
+          post_ident: post_ident,
           post_slug: post_slug
         });
 
